cf858ba10d1fa4d280cff2fdcb01da641fe2192b
Edwin Bosire <me@edwinb.co.uk>
2024-08-25 18:14:16 +0100

    Initial Commit

---
+++ UI-Experiments/UI_ExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_ExperimentsApp.swift
+//  UI-Experiments
+//
+//  Created by Edwin Bosire on 25/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct UI_ExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
